0058b77e78bc85d9385e0bd34729889c38846a29
Kristiyana Milcheva <kristiyana@kis619.42kmilchev>
2022-11-02 14:50:27 -0400

    new attempt. will go with debian

--- srcs/requirements/mariadb/conf/init_db.sh
+++
@@ -1,4 +0,0 @@
-chmod -R 777
-
-chown -R mysql:root /var/run/mysql
-exec mysqld --user=mysql --init-file=/initfile.sql
--- srcs/requirements_old/wordpress/conf/wp-config.php
+++ srcs/requirements_old/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', WORDPRESS_DB_NAME );
+define( 'DB_NAME', 'db_inception' );
 
 /** Database username */
-define( 'DB_USER', WORDPRESS_DB_USER );
+define( 'DB_USER', 'wpdbuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', WORDPRESS_DB_PASSWORD );
+define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
-define( 'DB_HOST', WORDPRESS_DB_HOST );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -79,7 +79,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
